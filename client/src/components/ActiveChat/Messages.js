70ab4ea44ac5923f91b37555940fc0b8f3e58bb0
Daniel <nights05.omegas@icloud.com>
2022-02-28 12:12:55 -0500

    other senders photo indicator of the last message they read in chat

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,18 +1,42 @@
 import React from 'react';
-import { Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box, Avatar } from '@material-ui/core';
 import { SenderBubble, OtherUserBubble } from '.';
 import moment from 'moment';
 
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-end',
+  },
+  avatar: {
+    height: 20,
+    width: 20,
+  },
+}));
+
 const Messages = (props) => {
-  const { messages, otherUser, userId } = props;
+  const classes = useStyles();
+  const { messages, otherUser, userId, lastRead } = props;
+
 
   return (
     <Box>
-      {messages.map((message) => {
+      {messages.map((message, i) => {
         const time = moment(message.createdAt).format('h:mm');
 
         return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
+          <Box className={classes.root} key={message.id}>
+            <SenderBubble  text={message.text} time={time} />
+            {lastRead === i && 
+              <Avatar
+                alt={otherUser.username}
+                src={otherUser.photoUrl}
+                className={classes.avatar}
+              />
+            }
+          </Box>
         ) : (
           <OtherUserBubble
             key={message.id}
